a14e3ae44cc7ade01d480c7b3d55f80db5013f18
lexguru1 <alexghiurutan@gmail.com>
2022-03-28 11:48:06 +0200

    state is run from redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 // import "./App.css";
 
-import { useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { deposit } from "./store/balance/actions";
-
+import { selectBalance } from "./store/balance/selectors";
 function App() {
   const dispatch = useDispatch();
-  const [balance, setBalance] = useState(0);
-
+  const balance = useSelector(selectBalance);
   return (
     <div className="App">
       <p>Balance: {balance}$</p>
